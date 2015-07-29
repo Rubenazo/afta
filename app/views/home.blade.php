cfc69cee0ab5dc4159809145d6befd409ee3b0f7
Ruben Gonzalez <rubenx_360@hotmail.com>
2015-07-29 17:58:08 -0430

    Modal

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -20,26 +20,54 @@
 
 	<div id="programs">
 		<h2>{{ Lang::get('text.pizza') }}</h2>
-		<div class="col-xs-6">
-			<img src="img/social/usa.png">
-			<h3>Private</h3>
-			<p>TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO</p>
+		<a class="enlace" data-toggle="modal" data-target="#myModal" data-route="{{ URL::to('progam/1')}}" data-title="pizza" data-price="" data-info="">
+			<div class="col-xs-6">
+				<img src="img/social/usa.png">
+				<h3>Private</h3>
+				<p>TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO</p>
+			</div>
+		</a>
+		<a class="enlace" data-toggle="modal" data-target="#myModal" data-route="{{ URL::to('progam/2')}}" data-title="cachapa" data-price="" data-info="">
+			<div class="col-xs-6">
+				<img src="img/social/usa.png">
+				<h3>Corporate</h3>
+				<p>TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO</p>
+			</div>
+		</a>
+		<a class="enlace" data-toggle="modal" data-target="#myModal" data-route="{{ URL::to('progam/3')}}" data-title="" data-price="" data-info="">
+			<div class="col-xs-6">
+				<img src="img/social/usa.png">
+				<h3>Commercial</h3>
+				<p>TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO</p>
+			</div>
+		</a>
+		<a class="enlace" data-toggle="modal" data-target="#myModal" data-route="{{ URL::to('progam/4')}}" data-title="" data-price="" data-info="">
+			<div class="col-xs-6">
+				<img src="img/social/usa.png">
+				<h3>Enthusiast</h3>
+				<p>TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO</p>
+			</div>
+		</a>
+
+		<!-- Modal -->
+		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		        <h4 class="modal-title" id="myModalLabel"></h4>
+		      </div>
+		      <div class="modal-body">
+		        <div class="info"></div>
+		        <div class="price"></div>
+		      </div>
+		      <div class="modal-footer">
+		        <a class="btn btn-primary pizza">Save changes</a>
+		      </div>
+		    </div>
+		  </div>
 		</div>
-		<div class="col-xs-6">
-			<img src="img/social/usa.png">
-			<h3>Corporate</h3>
-			<p>TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO</p>
-		</div>
-		<div class="col-xs-6">
-			<img src="img/social/usa.png">
-			<h3>Commercial</h3>
-			<p>TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO</p>
-		</div>
-		<div class="col-xs-6">
-			<img src="img/social/usa.png">
-			<h3>Enthusiast</h3>
-			<p>TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO</p>
-		</div>
+
 	</div>
 
 	<div id="gallery">
--- public/js/custom.js
+++ public/js/custom.js
@@ -5,5 +5,14 @@ $(document).ready(function(){
     	autoplay: true,
  		autoplaySpeed: 3500,
     });
+
+///////////////////////////////////////////////////////////////
+
+	$('.enlace').click(function(event) {
+		$('.pizza').attr('href',$(this).data('route'));
+		$('.info').html($(this).data('info'));
+		$('.price').html($(this).data('price'));
+		$('.modal-title').html($(this).data('title'));
+	});
 });
 
